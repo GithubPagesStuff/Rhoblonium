f18b38ef7462d86e47ab20794a721dc5903f8e4e
Aiden <aiden@aidens-macbook-pro.local>
2021-06-24 16:59:48 -0400

    Initial Commit

---
+++ Rhoblonium/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Rhoblonium
+//
+//  Created by Aiden on 6/24/21.
+//  Copyright © 2021 xeriviOS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
